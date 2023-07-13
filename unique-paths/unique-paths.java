51698e470ed02bbfd6bbc0c46126ddba3607e921
prajwalgowda19 <123195190+prajwalgowda19@users.noreply.github.com>
2023-07-13 19:55:47 +0530

    Time: 0 ms (100.00%) | Memory: 39.9 MB (7.08%) - LeetSync

--- unique-paths/unique-paths.java
+++ unique-paths/unique-paths.java
@@ -1,12 +1,29 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int N=m+n-2;
-        int r=m-1;
-        double res=1;
-        for(int i=1;i<=r;i++)
+        if(m==0 || n==0) 
+            return 0;
+        if(m==1 || n==1) 
+            return 1;
+        int[][] dp = new int[m][n];
+        //left column
+        for(int i=0; i<m; i++)
         {
-            res=res*(N-r+i)/i;
+            dp[i][0] = 1;
         }
-        return (int)res;
+        //top row
+        for(int j=0; j<n; j++)
+        {
+            dp[0][j] = 1;
+        }
+        //fill up the dp table
+        for(int i=1; i<m; i++)
+        {
+            for(int j=1; j<n; j++)
+            {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
     }
+   
 }
