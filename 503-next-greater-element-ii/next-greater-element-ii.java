9dc90a4dedc593d961398c6f62eed35a3bbe62e1
prajwalgowda19 <123195190+prajwalgowda19@users.noreply.github.com>
2023-07-22 12:01:37 +0530

    Time: 89 ms (7.86%) | Memory: 45.2 MB (35.06%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -0,0 +1,33 @@
+class Solution {
+      public int[] nextGreaterElements(int[] nums) {
+        int[] checker=new int[nums.length+nums.length];
+        int[] ans=new int[nums.length];
+        for(int i=0;i<nums.length;i++)
+        {
+            checker[i]=nums[i];
+            checker[i+nums.length]=nums[i];
+        }
+        for(int i=0;i<nums.length;i++)
+        {
+           int anss=checkit(nums[i],checker,i);
+           ans[i]=anss;
+
+        }
+        return ans;
+    }
+    int checkit(int ele,int[] checker,int start)
+    {
+        boolean flag=false;
+        for(int i=start;i<checker.length;i++)
+        {
+            if(ele==checker[i])
+                flag=true;
+            if(flag)
+            {
+                if(ele<checker[i])
+                    return checker[i];
+            }
+        }
+        return -1;
+    }
+}
